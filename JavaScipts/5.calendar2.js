07f682f1b9a857239cc519baccddbc00253a1389
Chai Pin Zheng <58126222+Ducksss@users.noreply.github.com>
2021-02-17 00:39:24 +0800

    Add files via upload

---
+++ JavaScipts/5.calendar2.js
@@ -0,0 +1,38 @@
+"use strict";
+var dayEvent = new Array();
+
+dayEvent[1] = "<br>High traffic*";
+dayEvent[2] = "<br>Not free!<br/>";
+dayEvent[3] = "";
+dayEvent[4] = "";
+dayEvent[5] = "";
+dayEvent[6] = "";
+dayEvent[7] = "";
+
+dayEvent[8] = "<br>High traffic*";
+dayEvent[9] = "<br>Not free!<br/>";
+dayEvent[10] = "";
+dayEvent[11] = "";
+dayEvent[12] = "";
+dayEvent[13] = "";
+dayEvent[14] = "";
+
+dayEvent[15] = "<br>High traffic*";
+dayEvent[16] = "<br>Not free!<br/>";
+dayEvent[17] = "";
+dayEvent[18] = "";
+dayEvent[19] = "";
+dayEvent[20] = "";
+dayEvent[21] = "";
+
+dayEvent[22] = "<br>High traffic*";
+dayEvent[23] = "<br>Not free!<br/>";
+dayEvent[24] = "";
+dayEvent[25] = "";
+dayEvent[26] = "";
+dayEvent[27] = "";
+dayEvent[28] = "";
+
+dayEvent[29] = "<br>High traffic*";
+dayEvent[30] = "<br>Not free!<br/>";
+dayEvent[31] = "";
